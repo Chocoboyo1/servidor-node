bb2a13dbcd4a6c3ac8aa2f727dea4e243fc59d94
Erian Santoy <leonardo88erian@gmail.com>
2026-01-29 14:49:51 -0600

    cambios

--- server.js
+++ server.js
@@ -7,12 +7,23 @@ const PORT = 3000;
 //Crear el servidor
 const servidor = http.createServer((req, res) => {
 
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('¡Hola desde el servidor Node.js!');
-
+//Manejo de diferentes rutas
+    if(req.url === '/'){
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Pagina de principal.</h1>');
+    } 
+    else if(req.url === '/acerca'){
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<p>Esta es la pagina de acerca de.</p>');
+    }
+    else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end('<h1>Pagina no encontrada.</h1>');
+    }
 });
 
 //Iniciar el servidor
 servidor.listen(PORT, () => {
     console.log(`Servidor ejecutandose en http://localhost:${PORT}`);
+
 });
